9c9d4ff5ecbffaf33ff119fb12d24ae8d7921179
Henk Berendsen <henkberendsen@cn108.science.ru.nl>
2025-10-15 11:23:08 +0200

    Delete Integral_attacks_on_prince.py and make it possible to use auxiliary functions in Prince.py individually; update README.md to reflect these changes

--- Prince.py
+++ Prince.py
@@ -1,6 +1,7 @@
 ## PRINCE reference implementation
-## Author: David Tvrdý
-## Last edited: 25.03.2022
+## Original Author: David Tvrdý
+## Fork Author: Henk Berendsen
+## Last edited: 15.10.2025
 
 
 ## ------------ References ------------- ##
@@ -19,188 +20,182 @@
 
 
 
-
+## ----------------------------------------------------------------------
+## Constants
+SBox = [0xb,0xf,0x3,0x2,0xa,0xc,0x9,0x1,0x6,0x7,0x8,0x0,0xe,0x5,0xd,0x4]
+InvSBox = [0xb,0x7,0x3,0x2,0xf,0xd,0x8,0x9,0xa,0x6,0x4,0x0,0x5,0xe,0xc,0x1]
+RC = [ ## round constants
+    [0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
+    [0x1, 0x3, 0x1, 0x9, 0x8, 0xa, 0x2, 0xe, 0x0, 0x3, 0x7, 0x0, 0x7, 0x3, 0x4, 0x4],
+    [0xa, 0x4, 0x0, 0x9, 0x3, 0x8, 0x2, 0x2, 0x2, 0x9, 0x9, 0xf, 0x3, 0x1, 0xd, 0x0],
+    [0x0, 0x8, 0x2, 0xe, 0xf, 0xa, 0x9, 0x8, 0xe, 0xc, 0x4, 0xe, 0x6, 0xc, 0x8, 0x9],
+    [0x4, 0x5, 0x2, 0x8, 0x2, 0x1, 0xe, 0x6, 0x3, 0x8, 0xd, 0x0, 0x1, 0x3, 0x7, 0x7],
+    [0xb, 0xe, 0x5, 0x4, 0x6, 0x6, 0xc, 0xf, 0x3, 0x4, 0xe, 0x9, 0x0, 0xc, 0x6, 0xc],
+    [0x7, 0xe, 0xf, 0x8, 0x4, 0xf, 0x7, 0x8, 0xf, 0xd, 0x9, 0x5, 0x5, 0xc, 0xb, 0x1],
+    [0x8, 0x5, 0x8, 0x4, 0x0, 0x8, 0x5, 0x1, 0xf, 0x1, 0xa, 0xc, 0x4, 0x3, 0xa, 0xa],
+    [0xc, 0x8, 0x8, 0x2, 0xd, 0x3, 0x2, 0xf, 0x2, 0x5, 0x3, 0x2, 0x3, 0xc, 0x5, 0x4],
+    [0x6, 0x4, 0xa, 0x5, 0x1, 0x1, 0x9, 0x5, 0xe, 0x0, 0xe, 0x3, 0x6, 0x1, 0x0, 0xd],
+    [0xd, 0x3, 0xb, 0x5, 0xa, 0x3, 0x9, 0x9, 0xc, 0xa, 0x0, 0xc, 0x2, 0x3, 0x9, 0x9],
+    [0xc, 0x0, 0xa, 0xc, 0x2, 0x9, 0xb, 0x7, 0xc, 0x9, 0x7, 0xc, 0x5, 0x0, 0xd, 0xd]
+]
 
 ## ----------------------------------------------------------------------
-## Message as a 64-bit integer, key as a 64-bit integer.
-def PrinceCore(key, message):
-    
-    SBox = [0xb,0xf,0x3,0x2,0xa,0xc,0x9,0x1,0x6,0x7,0x8,0x0,0xe,0x5,0xd,0x4]
-    InvSBox = [0xb,0x7,0x3,0x2,0xf,0xd,0x8,0x9,0xa,0x6,0x4,0x0,0x5,0xe,0xc,0x1]
-
-    ## round constants
-    RC = [
-        [0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0],
-        [0x1, 0x3, 0x1, 0x9, 0x8, 0xa, 0x2, 0xe, 0x0, 0x3, 0x7, 0x0, 0x7, 0x3, 0x4, 0x4],
-        [0xa, 0x4, 0x0, 0x9, 0x3, 0x8, 0x2, 0x2, 0x2, 0x9, 0x9, 0xf, 0x3, 0x1, 0xd, 0x0],
-        [0x0, 0x8, 0x2, 0xe, 0xf, 0xa, 0x9, 0x8, 0xe, 0xc, 0x4, 0xe, 0x6, 0xc, 0x8, 0x9],
-        [0x4, 0x5, 0x2, 0x8, 0x2, 0x1, 0xe, 0x6, 0x3, 0x8, 0xd, 0x0, 0x1, 0x3, 0x7, 0x7],
-        [0xb, 0xe, 0x5, 0x4, 0x6, 0x6, 0xc, 0xf, 0x3, 0x4, 0xe, 0x9, 0x0, 0xc, 0x6, 0xc],
-        [0x7, 0xe, 0xf, 0x8, 0x4, 0xf, 0x7, 0x8, 0xf, 0xd, 0x9, 0x5, 0x5, 0xc, 0xb, 0x1],
-        [0x8, 0x5, 0x8, 0x4, 0x0, 0x8, 0x5, 0x1, 0xf, 0x1, 0xa, 0xc, 0x4, 0x3, 0xa, 0xa],
-        [0xc, 0x8, 0x8, 0x2, 0xd, 0x3, 0x2, 0xf, 0x2, 0x5, 0x3, 0x2, 0x3, 0xc, 0x5, 0x4],
-        [0x6, 0x4, 0xa, 0x5, 0x1, 0x1, 0x9, 0x5, 0xe, 0x0, 0xe, 0x3, 0x6, 0x1, 0x0, 0xd],
-        [0xd, 0x3, 0xb, 0x5, 0xa, 0x3, 0x9, 0x9, 0xc, 0xa, 0x0, 0xc, 0x2, 0x3, 0x9, 0x9],
-        [0xc, 0x0, 0xa, 0xc, 0x2, 0x9, 0xb, 0x7, 0xc, 0x9, 0x7, 0xc, 0x5, 0x0, 0xd, 0xd]
-    ]
-
-    ## auxiliary functions 
-    
-    def AddRoundConst(number):
-        for i in range(16): A[i] = A[i] ^ RC[number][i]
-        return  
-    
-    
-    def AddKey():
-        for i in range(16): A[i] = A[i] ^ K[i]
-        return  
+## Auxiliary functions
+def AddRoundConst(number, A):
+    for i in range(16): A[i] = A[i] ^ RC[number][i]
+    return A
 
 
-    def SBoxLayer():
-        for i in range(16): A[i] = SBox[A[i]]
-        return       
-        
+def AddKey(A, K):
+    for i in range(16): A[i] = A[i] ^ K[i]
+    return A
 
-    def InvSBoxLayer():  
-        for i in range(16): A[i] = InvSBox[A[i]]
-        return  
 
-    
-    def MPrimeLayer():    
-        T = []
-        for i in range(16): T.append(0x0)
-        for i in range(2):                
-            T[0 + 12 * i] = ((A[1 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x8) + ((A[0 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x4) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[3 + 12 * i]) & 0x2) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[2 + 12 * i]) & 0x1)
-
-            T[1 + 12 * i] = ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[2 + 12 * i]) & 0x8) + ((A[1 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x4) + ((A[0 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x2) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[3 + 12 * i]) & 0x1)
-
-            T[2 + 12 * i] = ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[3 + 12 * i]) & 0x8) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[2 + 12 * i]) & 0x4) + ((A[1 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x2) + ((A[0 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x1)
-
-            T[3 + 12 * i] = ((A[0 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x8) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[3 + 12 * i]) & 0x4) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[2 + 12 * i]) & 0x2) + ((A[1 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x1)                        
-               
-            T[4 + 4 * i] = ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[6 + 4 * i]) & 0x8) + ((A[5 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x4) + ((A[4 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x2) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[7 + 4 * i]) & 0x1)
-
-            T[5 + 4 * i] = ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[7 + 4 * i]) & 0x8) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[6 + 4 * i]) & 0x4) + ((A[5 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x2) + ((A[4 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x1)
-
-            T[6 + 4 * i] = ((A[4 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x8) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[7 + 4 * i]) & 0x4) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[6 + 4 * i]) & 0x2) + ((A[5 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x1)
-
-            T[7 + 4 * i] = ((A[5 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x8) + ((A[4 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x4) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[7 + 4 * i]) & 0x2) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[6 + 4 * i]) & 0x1)              
-        
-        for i in range(16): A[i] = T[i]
-        return 
+def SBoxLayer(A):
+    for i in range(16): A[i] = SBox[A[i]]
+    return A
     
 
-    def ShiftRows():        
-        temp = A[1]
-        for i in range(3): A[1 + 4*i] = A[5 + 4*i]
-        A[13] = temp
-        
-        temp = A[2]
-        A[2] = A[10]
-        A[10] = temp
-        temp = A[6]
-        A[6] = A[14]
-        A[14] = temp
-        
-        temp = A[15]
-        for i in range(3): A[15 - 4*i] = A[11 - 4*i]
-        A[3] = temp        
-        return 
-      
+def InvSBoxLayer(A):  
+    for i in range(16): A[i] = InvSBox[A[i]]
+    return A
 
-    def InvShiftRows():
-        temp = A[13]
-        for i in range(3): A[13 - 4*i] = A[9 - 4*i]
-        A[1] = temp
-        
-        temp = A[2]
-        A[2] = A[10]
-        A[10] = temp
-        temp = A[6]
-        A[6] = A[14]
-        A[14] = temp
-        
-        temp = A[3]
-        for i in range(3): A[3 + 4*i] = A[7 + 4*i]
-        A[15] = temp        
-        return 
+
+def MPrimeLayer(A):    
+    T = []
+    for i in range(16): T.append(0x0)
+    for i in range(2):                
+        T[0 + 12 * i] = ((A[1 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x8) + ((A[0 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x4) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[3 + 12 * i]) & 0x2) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[2 + 12 * i]) & 0x1)
+
+        T[1 + 12 * i] = ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[2 + 12 * i]) & 0x8) + ((A[1 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x4) + ((A[0 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x2) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[3 + 12 * i]) & 0x1)
+
+        T[2 + 12 * i] = ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[3 + 12 * i]) & 0x8) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[2 + 12 * i]) & 0x4) + ((A[1 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x2) + ((A[0 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x1)
+
+        T[3 + 12 * i] = ((A[0 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x8) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[3 + 12 * i]) & 0x4) + ((A[0 + 12 * i] ^ A[1 + 12 * i] ^ A[2 + 12 * i]) & 0x2) + ((A[1 + 12 * i] ^ A[2 + 12 * i] ^ A[3 + 12 * i]) & 0x1)                        
+            
+        T[4 + 4 * i] = ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[6 + 4 * i]) & 0x8) + ((A[5 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x4) + ((A[4 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x2) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[7 + 4 * i]) & 0x1)
+
+        T[5 + 4 * i] = ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[7 + 4 * i]) & 0x8) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[6 + 4 * i]) & 0x4) + ((A[5 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x2) + ((A[4 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x1)
+
+        T[6 + 4 * i] = ((A[4 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x8) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[7 + 4 * i]) & 0x4) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[6 + 4 * i]) & 0x2) + ((A[5 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x1)
+
+        T[7 + 4 * i] = ((A[5 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x8) + ((A[4 + 4 * i] ^ A[6 + 4 * i] ^ A[7 + 4 * i]) & 0x4) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[7 + 4 * i]) & 0x2) + ((A[4 + 4 * i] ^ A[5 + 4 * i] ^ A[6 + 4 * i]) & 0x1)              
+    
+    for i in range(16): A[i] = T[i]
+    return A
+
+
+def ShiftRows(A):        
+    temp = A[1]
+    for i in range(3): A[1 + 4*i] = A[5 + 4*i]
+    A[13] = temp
+    
+    temp = A[2]
+    A[2] = A[10]
+    A[10] = temp
+    temp = A[6]
+    A[6] = A[14]
+    A[14] = temp
+    
+    temp = A[15]
+    for i in range(3): A[15 - 4*i] = A[11 - 4*i]
+    A[3] = temp        
+    return A
     
 
-    def MLayer():
-        MPrimeLayer()
-        ShiftRows()
-        return    
- 
+def InvShiftRows(A):
+    temp = A[13]
+    for i in range(3): A[13 - 4*i] = A[9 - 4*i]
+    A[1] = temp
+    
+    temp = A[2]
+    A[2] = A[10]
+    A[10] = temp
+    temp = A[6]
+    A[6] = A[14]
+    A[14] = temp
+    
+    temp = A[3]
+    for i in range(3): A[3 + 4*i] = A[7 + 4*i]
+    A[15] = temp        
+    return A
 
-    def InvMLayer():
-        InvShiftRows()
-        MPrimeLayer()
-        return 
-    
-    
-    def Round(number):    
-        SBoxLayer()
-        MLayer()
-        AddRoundConst(number)
-        AddKey() 
-        return  
-    
 
-    def InvRound(number):
-        AddKey() 
-        AddRoundConst(number)        
-        InvMLayer()
-        InvSBoxLayer() 
-        return 
-    
+def MLayer(A):
+    A = MPrimeLayer(A)
+    A = ShiftRows(A)
+    return A   
 
-    def CreateNibbles():
-      for i in range(16):
+
+def InvMLayer(A):
+    A = InvShiftRows(A)
+    A = MPrimeLayer(A)
+    return A
+
+
+def Round(number, A, K):    
+    A = SBoxLayer(A)
+    A = MLayer(A)
+    A = AddRoundConst(number, A)
+    A = AddKey(A, K) 
+    return A
+
+
+def InvRound(number, A, K):
+    A = AddKey(A, K) 
+    A = AddRoundConst(number, A)        
+    A = InvMLayer(A)
+    A = InvSBoxLayer(A) 
+    return A
+
+
+def CreateNibbles(message, key):       
+    A = []
+    K = [] 
+
+    for i in range(16):
         a = (message >> (60 - i * 4)) & 0xf
         k = (key >> (60 - i * 4)) & 0xf
         A.append(a)
         K.append(k)
-      return
+    return A, K
 
 
-    def IntegerFromNibbles(A):
-      ciphertext = 0x0
-      for i in range(16):
+def IntegerFromNibbles(A):
+    ciphertext = 0x0
+    for i in range(16):
         ciphertext = ciphertext ^ A[i]
         if i != 15: ciphertext = ciphertext << 4
-      return ciphertext
+    return ciphertext
 
-
-
-    ## -------------------------------------------
-    ## internal state and the key matrix        
-    A = []
-    K = []  
-  
-    CreateNibbles()
-        
+## ----------------------------------------------------------------------
+## Message as a 64-bit integer, key as a 64-bit integer.
+def PrinceCore(key, message):
+    ## internal state and the key matrix 
+    A, K = CreateNibbles(message, key)
+     
     ## key addition
-    AddKey()
+    A = AddKey(A, K)
     
     ## the first round constant
-    AddRoundConst(0)    
+    A = AddRoundConst(0, A)    
     
     ## forward rounds
-    for i in range(5): Round(i+1)
+    for i in range(5): A = Round(i+1, A, K)
        
     ## the middle part  
-    SBoxLayer()    
-    MPrimeLayer()    
-    InvSBoxLayer()
+    A = SBoxLayer(A)    
+    A = MPrimeLayer(A)    
+    A = InvSBoxLayer(A)
         
     ## backward rounds
-    for i in range(5): InvRound(i+6)
+    for i in range(5): A = InvRound(i+6, A, K)
         
     ## the last round constant
-    AddRoundConst(11)
+    A = AddRoundConst(11, A)
     
     ## key addition
-    AddKey()    
+    A = AddKey(A, K)    
         
     ## integer from nibbles   
     ciphertext = IntegerFromNibbles(A)      
--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # PRINCE
-A Python 3 reference implementation of the lightweight block cipher PRINCE and an example of several integral attacks on round reduced version of PRINCE.
+A Python 3 reference implementation of the lightweight block cipher PRINCE. This fork modifies the code to use the state `A` as a function argument instead of a global variable, making it possible to use helper functions individually.
 
 The implementation works on the test vectors provided by the authors of the cipher. It is a straightforward, unoptimized implementation.
 No effort has been made to defend against side-channel or any other kind of attacks.
